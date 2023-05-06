64003078d76ba22eedbb287942e0b12a6d12b510
alulcode <alulgans70@gmail.com>
2023-05-06 08:22:08 +0700

    Apa saja yang berubah?: 	deleted:    assets/profile.png 	deleted:    contact.html 	modified:   inc/config.php 	deleted:    inc/eksekusi.php 	deleted:    projects.html 	deleted:    resume.html

--- inc/config.php
+++ inc/config.php
@@ -1,7 +1,7 @@
 <?php
-$host = "127.0.0.1";
-$user = "root";
-$pass = "";
-$data = "smkn4bojonegoro_kelulusan";
+$host = "127.0.0.1"; // rubah ini sesuai dengan host anda
+$user = "root";  // rubah ini sesuai dengan username anda
+$pass = "";  // rubah ini sesuai dengan password anda
+$data = "smkn4bojonegoro_kelulusan"; // rubah ini sesuai dengan nama database anda
 $conn = mysqli_connect($host, $user, $pass, $data);
 mysqli_select_db($conn, $data);
